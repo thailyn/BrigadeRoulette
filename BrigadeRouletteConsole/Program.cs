6fdab7bd2d2134cdd452699832685d587c771c67
Charles Macanka <cmacanka@gmail.com>
2015-02-21 17:44:04 -0500

    Compute trivial win percent for each formation.
    For each five-familiar formation, compute the win percentage, using each
    input familiar in the corresponding position (left to right) (that is,
    do not change the order of the familiars).

--- BrigadeRouletteConsole/Program.cs
+++ BrigadeRouletteConsole/Program.cs
@@ -62,42 +62,81 @@ namespace BrigadeRouletteConsole
         static void PrintBrigadeFormations(PhlebotomistRepository phlebotomistRepository,
             IQueryable<BrigadeFormation> formations)
         {
-            var rows = phlebotomistRepository.Context.BrigadeFormationVerticalPositionTypes.OrderByDescending(x => x.DamageDealtModifier);
             foreach (var brigadeFormation in formations)
             {
-                System.Console.WriteLine("Brigade Formation: {0}", brigadeFormation.Name);
-                foreach (var row in rows)
+                PrintBrigadeFormation(phlebotomistRepository, brigadeFormation);
+            }
+        }
+
+        static void PrintBrigadeFormation(PhlebotomistRepository phlebotomistRepository, BrigadeFormation formation)
+        {
+            var rows = phlebotomistRepository.Context.BrigadeFormationVerticalPositionTypes.OrderByDescending(x => x.DamageDealtModifier);
+            System.Console.WriteLine("Brigade Formation: {0}", formation.Name);
+            foreach (var row in rows)
+            {
+                // using the assumption that HorizontalPositionTypeId increases from left to right
+                foreach (var horizontalPosition in formation.Positions.OrderBy(x => x.HorizontalPositionTypeId))
                 {
-                    // using the assumption that HorizontalPositionTypeId increases from left to right
-                    foreach (var horizontalPosition in brigadeFormation.Positions.OrderBy(x => x.HorizontalPositionTypeId))
+                    if (horizontalPosition.VerticalPositionTypes.Id == row.Id)
                     {
-                        if (horizontalPosition.VerticalPositionTypes.Id == row.Id)
-                        {
-                            System.Console.Write("*");
-                        }
-                        else
-                        {
-                            System.Console.Write(" ");
-                        }
-                        //System.Console.WriteLine("Horizontal: '{0}'/{1}, Vertical: '{2}'", horizontalPosition.HorizontalPositionTypes.Name,
-                        //    horizontalPosition.HorizontalPositionTypeId, horizontalPosition.VerticalPositionTypes.Name);
+                        System.Console.Write("*");
                     }
-                    System.Console.WriteLine();
+                    else
+                    {
+                        System.Console.Write(" ");
+                    }
+                    //System.Console.WriteLine("Horizontal: '{0}'/{1}, Vertical: '{2}'", horizontalPosition.HorizontalPositionTypes.Name,
+                    //    horizontalPosition.HorizontalPositionTypeId, horizontalPosition.VerticalPositionTypes.Name);
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+        static double CalculateBrigadeFormationWinPercent(List<FamiliarWinPercent> familiarWinPercents, BrigadeFormation formation)
+        {
+            double formationWinPercent = 0;
+            double totalWinPercent = 0;
+            bool includeReserve = true;
+
+            int numFormationPositions = formation.Positions.Count;
+            int currentFamiliarIndex = 0;
+            foreach (var position in formation.Positions)
+            {
+                totalWinPercent += familiarWinPercents[currentFamiliarIndex].WinPercents[position.VerticalPositionTypes];
+                currentFamiliarIndex++;
+            }
+            if (includeReserve)
+            {
+                foreach (var position in formation.Positions)
+                {
+                    totalWinPercent += familiarWinPercents[currentFamiliarIndex].WinPercents[position.VerticalPositionTypes];
+                    currentFamiliarIndex++;
                 }
             }
+            System.Console.WriteLine("Total win percent{0}: {1}", (includeReserve ? " (with reserve)" : string.Empty),
+                totalWinPercent);
+
+            formationWinPercent = totalWinPercent / (includeReserve ? numFormationPositions * 2 : numFormationPositions);
+            return formationWinPercent;
         }
 
         static void Main(string[] args)
         {
-            ReadInputFile(_inputFileName);
-
             PhlebotomistModelContainer brigadeContext = new PhlebotomistModelContainer();
             PhlebotomistRepository phlebotomistRepository = new Phlebotomist.Repositories.PhlebotomistRepository(brigadeContext);
 
+            var familiarWinPercents = ReadInputFile(phlebotomistRepository, _inputFileName);
+
             var fiveFamiliarBrigadeFormations = phlebotomistRepository.Context.BrigadeFormations.Where(x => x.NumPositions == 5);
             System.Console.WriteLine("Found {0} brigade formation with five familiars.", fiveFamiliarBrigadeFormations.Count());
-            PrintBrigadeFormations(phlebotomistRepository, fiveFamiliarBrigadeFormations);
-
+            foreach (var brigadeFormation in fiveFamiliarBrigadeFormations)
+            {
+                PrintBrigadeFormation(phlebotomistRepository, brigadeFormation);
+                double formationWinPercent = CalculateBrigadeFormationWinPercent(familiarWinPercents,
+                    brigadeFormation);
+                System.Console.WriteLine("Win percent for formation '{0}': {1}", brigadeFormation.Name,
+                    formationWinPercent);
+            }
 
             System.Console.WriteLine("Found {0} familiar types.", phlebotomistRepository.Context.FamiliarTypes.Count());
             foreach (var familiarType in phlebotomistRepository.Context.FamiliarTypes)
